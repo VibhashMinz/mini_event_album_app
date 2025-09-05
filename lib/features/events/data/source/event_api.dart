75e6dd9fcd0c5e67330bed78ea8797993372644f
vibhash <vibhashgyanminz23@gmail.com>
2025-09-05 17:23:32 +0530

    feat(events): implement EventsApi with Dio and ApiException handling

---
+++ lib/features/events/data/source/event_api.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:mini_event_album_app/core/dio_client.dart';
+
+import '../../../../core/api_exceptions.dart';
+import '../../domain/entities/event.dart';
+import 'events_api_base.dart';
+
+class EventsApi implements EventsApiBase {
+  EventsApi({required DioClient dioClient}) : _dioClient = dioClient;
+
+  final DioClient _dioClient;
+
+  @override
+  Future<List<Event>> getEvents({int page = 1, int limit = 10}) async {
+    try {
+      final response = await _dioClient.dio.get(
+        '/events',
+        queryParameters: {'page': page, 'limit': limit},
+      );
+      final List<dynamic> data = response.data;
+      return data.map((json) => Event.fromJson(json)).toList();
+    } on DioException catch (e) {
+      throw ApiException.fromDio(e);
+    } catch (e) {
+      throw ApiException(message: 'Unexpected error: $e', originalError: e);
+    }
+  }
+}
